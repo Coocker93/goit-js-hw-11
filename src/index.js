3c50527030e00692c887c0d7a7d5c18ea67bf60f
Andrew Povar <122461768+Coocker93@users.noreply.github.com>
2023-06-16 14:11:37 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,119 +1,101 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const loadMoreBtn = document.querySelector('.load-more');
-  
-  async function searchImages(event) {
-    event.preventDefault();
+const loadMoreBtn = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
 
-    const searchQuery = document.querySelector('input[name="searchQuery"]').value;
+async function searchImages(event) {
+  event.preventDefault();
 
-    try {
-      const response = await axios.get('https://pixabay.com/api/', {
-        params: {
-          key: '37252782-ab1fd22b6d95033438d9d3935',
-          q: searchQuery,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          per_page: 40,
-        },
-      });
+  const searchQuery = document.querySelector('input[name="searchQuery"]').value;
 
-      const images = response.data.hits;
-      const totalHits = response.data.totalHits;
-
-      if (images.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        renderImages(images);
-        if (images.length < totalHits) {
-          loadMoreBtn.style.display = 'block';
-        } else {
-          loadMoreBtn.style.display = 'none';
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      Notiflix.Notify.failure('An error occurred while fetching images. Please try again later.');
-    }
-  }
-
-  function renderImages(images) {
-    const gallery = document.querySelector('.gallery');
-    gallery.innerHTML = '';
-
-    images.forEach((image) => {
-      const card = createImageCard(image);
-      gallery.appendChild(card);
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '37252782-ab1fd22b6d95033438d9d3935',
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+      },
     });
-  }
 
-  function createImageCard(image) {
-    const card = document.createElement('div');
-    card.classList.add('photo-card');
+    const images = response.data.hits;
+    const totalHits = response.data.totalHits;
 
-    const img = document.createElement('img');
-    img.src = image.webformatURL;
-    img.alt = image.tags;
-    img.loading = 'lazy';
-
-    const info = document.createElement('div');
-    info.classList.add('info');
-
-    const likes = createInfoItem('Likes', image.likes);
-    const views = createInfoItem('Views', image.views);
-    const comments = createInfoItem('Comments', image.comments);
-    const downloads = createInfoItem('Downloads', image.downloads);
-
-    info.append(likes, views, comments, downloads);
-    card.append(img, info);
-
-    return card;
-  }
-
-  function createInfoItem(label, value) {
-    const item = document.createElement('p');
-    item.classList.add('info-item');
-    item.innerHTML = `<b>${label}</b>: ${value}`;
-
-    return item;
-  }
-
-  async function loadMoreImages() {
-    const searchQuery = document.querySelector('input[name="searchQuery"]').value;
-    const currentPage = Math.ceil(document.querySelectorAll('.photo-card').length / 40) + 1;
-
-    try {
-      const response = await axios.get('https://pixabay.com/api/', {
-        params: {
-          key: '37252782-ab1fd22b6d95033438d9d3935',
-          q: searchQuery,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          per_page: 40,
-          page: currentPage,
-        },
-      });
-
-      const images = response.data.hits;
+    if (images.length === 0) {
+      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    } else {
       renderImages(images);
-
-      const totalHits = response.data.totalHits;
-      if (images.length >= totalHits) {
+      if (images.length < totalHits) {
+        loadMoreBtn.style.display = 'block';
+      } else {
         loadMoreBtn.style.display = 'none';
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
       }
-    } catch (error) {
-      console.error(error);
-      Notiflix.Notify.failure('An error occurred while fetching images. Please try again later.');
     }
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure('An error occurred while fetching images. Please try again later.');
   }
+}
 
-  document.querySelector('#search-form').addEventListener('submit', searchImages);
-  loadMoreBtn.addEventListener('click', loadMoreImages);
-});
+function renderImages(images) {
+  images.forEach((image) => {
+    const card = createImageCard(image);
+    gallery.insertAdjacentHTML('beforeend', card);
+  });
+}
+
+function createImageCard(image) {
+  const card = `
+    <div class="photo-card">
+      <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy">
+      <div class="info">
+        ${createInfoItem('Likes', image.likes)}
+        ${createInfoItem('Views', image.views)}
+        ${createInfoItem('Comments', image.comments)}
+        ${createInfoItem('Downloads', image.downloads)}
+      </div>
+    </div>
+  `;
+
+  return card;
+}
+
+function createInfoItem(label, value) {
+  return `<p class="info-item"><b>${label}</b>: ${value}</p>`;
+}
+
+async function loadMoreImages() {
+  const searchQuery = document.querySelector('input[name="searchQuery"]').value;
+  const currentPage = Math.ceil(document.querySelectorAll('.photo-card').length / 40) + 1;
+
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '37252782-ab1fd22b6d95033438d9d3935',
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: currentPage,
+      },
+    });
+
+    const images = response.data.hits;
+    renderImages(images);
+
+    const totalHits = response.data.totalHits;
+    if (currentPage * 40 >= totalHits) {
+      loadMoreBtn.style.display = 'none';
+    }
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure('An error occurred while fetching images. Please try again later.');
+  }
+}
+
+document.querySelector('#search-form').addEventListener('submit', searchImages);
+loadMoreBtn.addEventListener('click', loadMoreImages);
